59f54f5111d272cae093d490db6cb28f3bbf38aa
sakibnazmus1 <sakibnazmus438@gmail.com>
2022-12-03 00:06:24 +0600

    make a simple api

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const catagories = require('./data/catagories.json');
+const news = require('./data/news.json');
 
 app.get('/', (req, res) => {
     res.send('News Api Running');
@@ -13,6 +14,24 @@ app.get('/', (req, res) => {
 
 app.get('/news-catagories', (req, res) => {
     res.send(catagories);
+})
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '08') {
+        res.send(news);
+    }
+    else {
+        const catagoryNews = news.filter(n => n.category_id === id);
+        res.send(catagoryNews);
+    }
+
+})
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews);
 })
 
 app.listen(port, () => {
